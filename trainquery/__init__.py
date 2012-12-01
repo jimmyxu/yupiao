ef1d6312ddc6bd3a2d66bb9b818d4158f7c6b4d6
Jimmy Xu <me@jimmyxu.org>
2012-12-01 01:37:16 +0000

    增加train_no过滤、始发终到站
    Signed-off-by: Jimmy Xu <me@jimmyxu.org>

--- trainquery/__init__.py
+++ trainquery/__init__.py
@@ -21,6 +21,8 @@ class TrainQuery:
         self.s = requests.session()
         self.s.config['base_headers']['User-Agent'] = \
             'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
+        self.s.config['base_headers']['Referer'] = base % 'method=init'
+        self.s.config['base_headers']['X-Requested-With'] = 'XMLHttpRequest'
         # FIXME: refine later
         telecode = lambda z: (station_name.has_key(z.upper()) and
                               (station_name[z.upper()], False) or (z, True))
@@ -36,19 +38,37 @@ class TrainQuery:
 
 
         r = self.s.get(base % 'method=init')
+
+        payload = {'date': str(self.date),
+                   'fromstation': station_name_rev[self.fz],
+                   'tostation': station_name_rev[self.dz],
+                   'starttime': '00:00--24:00',}
+        # TODO: switch to base when ready
+        #r = self.s.post(base % 'method=queryststrainall', data=payload)
+        r = self.s.post('https://www.12306.cn/otsweb/order/querySingleAction.do?method=queryststrainall', data=payload, verify=False)
+        tj = r.json
+        trains = {}
+        for train in tj:
+            for code in train['value'].split('/'):
+                trains[code] = {'start': train['start_station_name'].encode('utf-8'),
+                                'end': train['end_station_name'].encode('utf-8'),
+                                'id': train['id'].encode('utf-8')}
+        trainno = ''
+        if self.traincode:
+            try:
+                trainno = trains[self.traincode]['id']
+            except KeyError:
+                return {}
         payload = {'orderRequest.train_date': str(self.date),
                    'orderRequest.from_station_telecode': station_name_rev[self.fz],
                    'orderRequest.to_station_telecode': station_name_rev[self.dz],
-                   'orderRequest.train_no': '', # FIXME: self.traincode,
+                   'orderRequest.train_no': trainno,
                    'trainPassType': 'QB',
                    'trainClass': 'QB#D#Z#T#K#QT#',
                    'includeStudent': '00',
                    'seatTypeAndNum': '',
                    'orderRequest.start_time_str': '00:00--24:00',}
-        headers = {'Referer': base % 'method=init',
-                   'X-Requested-With': 'XMLHttpRequest',}
-        r = self.s.get(base % 'method=queryLeftTicket',
-                       params=payload, headers=headers)
+        r = self.s.get(base % 'method=queryLeftTicket', params=payload)
         t = r.json['datas'].encode('utf-8')
 
         result = {}
@@ -56,14 +76,17 @@ class TrainQuery:
         for line in t.split(r'\n'):
             text = re.sub(r'<.+?>', '', line).replace('&nbsp;', '')
             text = re.sub(r',无(?=,)', ',0', text).split(',')
+
             try:
-                text = text[:2] + [text[2][:-5]] + [text[3][:-5]] + \
+                tq = '%s(%s->%s)' % (text[1],
+                        trains[text[1]]['start'], trains[text[1]]['end'])
+            except KeyError:
+                tq = text[1]
+            try:
+                text = [text[0]] + [tq] + [text[2][:-5]] + [text[3][:-5]] + \
                        [text[2][-5:]] + [text[3][-5:]] + text[4:]
             except IndexError:
                 continue # TODO: why?
-
-            if self.traincode and not self.traincode in text[1]:
-                continue
 
             if ((self.fztc or text[2] == self.fz) and
                 (self.dztc or text[3] == self.dz)):
@@ -74,7 +97,7 @@ class TrainQuery:
 
 
 if __name__ == '__main__':
-    tq = TrainQuery('BJP', 'SHH', date=today + datetime.timedelta(8))
+    tq = TrainQuery('XAY', 'WXH', traincode='T140', date=today + datetime.timedelta(8))
     data = tq.query()
     for d in sorted(data.keys()):
         print ('%d\t%s' % (d, str(data[d]))).decode("string_escape")
